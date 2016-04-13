7fcda5b7631673995ede7861c40b3934f818934f
Wendy Yang <atlwendy@gmail.com>
2016-04-13 09:42:00 -0700

    added setup.js to be required by mocha

--- hello.jsx
+++ hello.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default class Hello extends React.Component {
 
+  handleChange() {
+    let text = React.findDOMNode(this.refs.input).value.trim();
+    React.findDOMNode(this.refs.result).value = text;
+  }
+
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+          <h1>Hello world</h1>
+
+          <input type="text" ref="input" placeholder="NAME" onChange={this.handleChange.bind(this)} />
+          <div ref="result"> </div>
+      </div>
+    );
   }
 
 }
--- test/tests.js
+++ test/tests.js
@@ -4,16 +4,22 @@ const TestUtils = addons.addons.TestUtils;
 import React from 'react';
 var jsdom = require('jsdom');
 import assert from 'assert';
+import ror from 'react-on-rails';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.parentWindow;
+describe('render correctly', function() {
 
-describe('blah', function() {
-
-  it('should work', function() {
+  it('should render', function() {
     let comp = TestUtils.renderIntoDocument(<Hello/>);
     let h1 = TestUtils.findRenderedDOMComponentWithTag(comp, 'h1');
     assert.equal(h1.getDOMNode().textContent, 'Hello world');
   });
+
+  it('should change input field', function(){
+  	let comp = TestUtils.renderIntoDocument(<Hello/>);
+  	let input = TestUtils.findRenderedDOMComponentWithTag(comp, 'input');
+  	input.value = 'Wendy';
+  	TestUtils.Simulate.change(input);
+  	TestUtils.Simulate.keyDown(input, { key: "Enter", keyCode: 13, which: 13 });
+  })
 
 });
